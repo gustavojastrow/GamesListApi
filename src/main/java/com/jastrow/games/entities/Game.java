e7e625b0f4b61181adae5301a00096563e1d5d55
Gustavo Jastrow <jaastrow@hotmail.com>
2023-07-14 20:21:33 -0300

    Database Population

--- src/main/java/com/jastrow/games/entities/Game.java
+++ src/main/java/com/jastrow/games/entities/Game.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "tb_games")
+@Table(name = "tb_game")
 @Setter
 @Getter
 @NoArgsConstructor
@@ -24,13 +24,17 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
